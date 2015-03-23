4032ce2e57a299c90ba56e6b2a0bfdea842e54e9
pdn <pnathan@vandals.uidaho.edu>
2015-03-22 23:29:41 -0700

    Add enough skeleton to get a compile running

--- jasko.tim.lisp/src/jasko/tim/lisp/editors/actions/CallUrlAction.java
+++ jasko.tim.lisp/src/jasko/tim/lisp/editors/actions/CallUrlAction.java
@@ -1,1 +1,9 @@
-// nulled out. This file has to be rewritten.
+package jasko.tim.lisp.editors.actions;
+
+public class CallUrlAction extends LispAction {
+
+   public CallUrlAction(Object object, String string) {
+      // TODO Auto-generated constructor stub
+   }
+   
+}
--- jasko.tim.lisp/src/jasko/tim/lisp/editors/actions/HyperSpecAction.java
+++ jasko.tim.lisp/src/jasko/tim/lisp/editors/actions/HyperSpecAction.java
@@ -1,1 +1,10 @@
-// nulled out. This file has to be rewritten.
+package jasko.tim.lisp.editors.actions;
+
+public class HyperSpecAction extends CallUrlAction {
+
+   public HyperSpecAction(Object object, String string) {
+      super(object, string);
+      // TODO Auto-generated constructor stub
+   }
+   
+}
